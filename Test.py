dd10b028d759d835341294b9b1ef617eea2a22a3
Piyush <Piyush.Dharkar@gmail.com>
2018-01-21 16:35:35 +0530

    Cleaned up code.

--- Test.py
+++ Test.py
@@ -9,10 +9,11 @@ dataset = originalDataset.drop('Unnamed: 32', axis=1)
 dataset['diagnosis'] = dataset['diagnosis'].map({'B':0, 'M':1})
 datasetCorr = dataset.corr()
 
-#print("Corelations:- \n")
-#print(datasetCorr['diagnosis'].sort_values(ascending=False))
-#print("\n")
+print("Corelations:- \n")
+print(datasetCorr['diagnosis'].sort_values(ascending=False))
+print("\n")
 
+#Selected 14 most relevant features
 selectedFeatures = ['concave points_worst', 'perimeter_worst', 
                     'concave points_mean', 'perimeter_worst', 
                     'concave points_mean', 'radius_worst',
